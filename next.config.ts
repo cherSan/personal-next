9c6d381bdc85a2e9fa2323e294b7c154169fce3c
chersan <chernushevich.alexander@gmail.com>
2025-10-21 22:29:51 +0700

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: undefined,
+    images: { unoptimized: false },
+    basePath: '',
 };
 
 export default nextConfig;
